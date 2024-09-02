444f67617e8976cb26e3d9b5ee2da28574e72b32
karim <hema_thechamp@yahoo.com>
2024-09-02 19:33:36 -0400

    init chapter2

---
+++ chapter2.js
@@ -0,0 +1,9 @@
+let intrestRate = 0.06;
+intrestRate = intrestRate / 12;
+document.write(intrestRate);
+//
+document.write('<br>')
+//
+let firstName;
+firstName = prompt('Enter your first name :', 'Human');
+document.write('Welcome ' + firstName)
