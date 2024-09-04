bd5eaacae46c82b79d195371d30d912224ab5c8f
karim <hema_thechamp@yahoo.com>
2024-09-03 23:32:50 -0400

    while loop practice

--- chapter4.js
+++ chapter4.js
@@ -6,17 +6,18 @@
 // }
 // countdown(8);
 
-function sumOfEvens(arr) {
-    let sum = 0; //init sum of evens
-    let i = 0; //value for the arr index
-    while (i < arr.length) {
-        if (arr[i] % 2 === 0) {
-            sum += arr[i];
-        }
-        i++;
-    }
+// function sumOfEvens(arr) {
+//     let sum = 0; //init sum of evens
+//     let i = 0; //value for the arr index
+//     while (i < arr.length) {
+//         if (arr[i] % 2 === 0) {
+//             sum += arr[i];
+//         }
+//         i++;
+//     }
 
-    return sum;
-}
+//     return sum;
+// }
 
-console.log(sumOfEvens([1, 2, 3, 4, 5, 6, 7, 8]));
+// console.log(sumOfEvens([1, 2, 3, 4, 5, 6, 7, 8]));
+
