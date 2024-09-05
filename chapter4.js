657086993e1330c251388e2daf29702a67709e26
karim <hema_thechamp@yahoo.com>
2024-09-05 13:35:34 -0400

    for, while, and dowhile practice

--- chapter4.js
+++ chapter4.js
@@ -66,16 +66,154 @@
 
 // console.log(countVowels('Shakespeare'));
 
-function reverseString(string) {
-    let reversed = ""; //init an empty string to hold the reversed result
-    let i = string.length - 1; // start at the last character of the string
+// function reverseString(string) {
+//     let reversed = ""; //init an empty string to hold the reversed result
+//     let i = string.length - 1; // start at the last character of the string
 
-    while (i >= 0) { //loop backward through the string
-        reversed += string[i]; // add each character to the reversed string
-        i--; // move to the prev character
-    }
+//     while (i >= 0) { //loop backward through the string
+//         reversed += string[i]; // add each character to the reversed string
+//         i--; // move to the prev character
+//     }
     
-    return reversed; //return the reversed string
+//     return reversed; //return the reversed string
+// }
+
+// console.log(reverseString('Kristy'));
+
+
+//////for loop/////
+
+// let sum = 0;
+// let num;
+
+// for (let counter = 1; counter <= 3; counter += 1) {
+//     num = prompt("type a number ", 1);
+//     sum += Number(num);
+
+// }
+// console.log("The total of your numbers is : " + sum);
+
+/////
+
+// let sum = 0;
+// for (let counter = 1; counter < 4; counter += 1) {
+//     let num;
+//     let ordinal;
+
+//     switch (counter) {
+//         case 1:
+//             ordinal= "first";
+//             break;
+//         case 2:
+//             ordinal = "second";
+//             break;
+//         case 3:
+//             ordinal ="third";
+//     }
+//     num = prompt("enter the  " + ordinal + " number:", 1);
+//     sum += Number(num)
+// }
+// console.log("The average is " + sum / 3);
+
+///
+
+// for (let i = 3; i >= 1; i -= 1) {
+//     let ordinal;
+//     let color;
+
+//     switch (i) {
+//         case 1:
+//             ordinal = 'first';
+//             break;
+//         case 2:
+//             ordinal = 'second';
+//             break;
+//         case 3:
+//             ordinal = 'third';
+//     }
+//     color = prompt("Enter your " + ordinal + " color", '')
+//     console.log(i + '. ' + color)
+
+//     }
+
+////do...while//
+
+//in the code below the user can opt out of answering from the very first prompt
+
+// let sum = 0;
+// let num = prompt("type a number or click Cancel:", 1);
+// while (num != null) {
+//     sum += Number(num);
+//     num = prompt("type a number or click cancel: ", 1);
+// }
+
+///
+
+//in the code below sum and num are init to 0 and the while loop ensures that the prompt runs at least once
+//evenn if the user clicks cancel the th value is at 0 instead of null; however the do while speciaalizes in that instead
+
+// let sum = 0;
+// let num = 0;
+// while (num !== null || sum === 0) {
+//     num = prompt("type a number; when youre done, click cancel", 1);
+//     sum += Number(num);
+// }
+// console.log("total is " + sum);
+
+//
+// let sum = 0;
+// let num;
+
+// do {
+//     num = prompt("type a number; when you're done, click Cancel: ", 1)
+//     sum += Number(num);
+// }
+// while (num !== null || sum === 0);
+// console.log( 'the total number of your number are ' + sum);
+
+
+//for loop practice/////////////////
+
+// function sumOfSquares(arr) {
+//     let sum = 0;
+//     for (i = 0; i < arr.length; i++) {
+//         sum += arr[i] * arr[i];     
+//     }
+//     return sum;
+// }
+
+// console.log(sumOfSquares([1, 2, 4]));
+
+
+//////////
+
+
+// function countDivisibleByThree(arr) {
+//     sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 3 ===0) {
+//             sum++
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(countDivisibleByThree([1, 2, 3, 4, 5, 6]));
+
+
+////do...while practice////
+
+function findFirstNegative(arr) {
+    let i = 0; //initialze index;
+    let firstNegative = null; //initialize variable to store the first negative #
+    do {
+        if (arr[i] < 0) { //check if the current number is negative
+            firstNegative = arr[i]//store the first negative #
+            break;//exit the loop once the first negative # is found
+        }
+        i++; //increment index
+ } while (i < arr.length); //continue looping while there are elements in the array
+    return firstNegative;//return the first negative # or null if none is found
 }
 
-console.log(reverseString('Kristy'));
+console.log(findFirstNegative([-1, 3, 5]))
