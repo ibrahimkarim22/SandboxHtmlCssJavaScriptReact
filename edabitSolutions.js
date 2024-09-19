e166cc7230e27fe8c5dfc7af58ec1d444896cd4e
karim <hema_thechamp@yahoo.com>
2024-09-19 19:59:43 -0400

    solved a few more edabit challenges

--- edabitSolutions.js
+++ edabitSolutions.js
@@ -23,3 +23,35 @@ function boolToString(flag) {
 }
 console.log(boolToString(true));
 console.log(boolToString(false));
+
+//https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG
+
+function sumPolygon(n) {
+  return (
+    (n - 2) * 180
+    )
+}
+console.log(sumPolygon(7));
+//////////////////////////////
+
+//https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3
+function checkEquality(a, b) {
+  if (a === b) {
+		return true;
+	} else return false;
+}
+console.log(checkEquality(true, 1));
+console.log(checkEquality(1, '1'));
+console.log(checkEquality(1, 1));
+/////////////////////////////////
+
+//https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3
+function maxNum(n1, n2) {
+	if (n1 > n2) {
+	  return n1
+	} else if (n1 < n2) {
+		return n2
+	}
+}
+console.log(maxNum(2, 1));
+console.log(maxNum(1, 2));
