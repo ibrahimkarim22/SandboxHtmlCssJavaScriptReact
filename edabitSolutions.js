24c0782821fb2e616efd8ca2d913ab3f50180645
karim <hema_thechamp@yahoo.com>
2024-09-18 20:19:24 -0400

    few more edabit problems

--- edabitSolutions.js
+++ edabitSolutions.js
@@ -1,9 +1,25 @@
 
 //  https://edabit.com/challenge/GwvwXHWCThHZrR7xu 
    
-  function footballPoints(wins, draws, losses) {
-    let points = (wins * 3) + (draws * 1) + (losses * 0);
-    return points;
-  }
+  // function footballPoints(wins, draws, losses) {
+  //   let points = (wins * 3) + (draws * 1) + (losses * 0);
+  //   return points;
+  // }
 
-  console.log(footballPoints(3, 2, 3));
+  // console.log(footballPoints(3, 2, 3));
+/////////////////////////
+
+//https://edabit.com/challenge/FipbQSYquQLPZ8QXG
+function isSeven(x) {
+	return x === 7 ? true : false;
+}
+console.log(isSeven(7));
+console.log(isSeven(8)); 
+////////////////////////////////////////
+
+//https://edabit.com/challenge/KSTkFSnaYBJdo6PHx
+function boolToString(flag) {
+  return flag.toString();
+}
+console.log(boolToString(true));
+console.log(boolToString(false));
