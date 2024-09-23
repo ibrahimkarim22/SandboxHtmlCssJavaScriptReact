704373491cd9d29d03ce659ce50ebc385df72ac1
karim <hema_thechamp@yahoo.com>
2024-09-22 21:18:07 -0400

    practice a flatten function with object and array with tests

--- sandBox.js
+++ sandBox.js
@@ -22,7 +22,6 @@
 // console.log(sumOfEvens([1, 2, 3, 4, 5, 6, 7, 8]));
 //////////af
 
-
 // function findMax(arr) { //init arr variable to hold array of integers
 //     let max = arr[0]; // initialize max to the first element in the array
 //     let i = 1; // start looping from the second element (index 1)
@@ -30,9 +29,9 @@
 //         if (arr[i] > max) { //if the current elemnt is greater than the current max
 //             max = arr[i] // update max to the current element
 //         }
-//         i++ // move to the next element 
+//         i++ // move to the next element
 //     }
-//     return max; //Return the largest value found 
+//     return max; //Return the largest value found
 // }
 // console.log(findMax([3, 5, 7, 2, 8, -1, 4, 10, 12]));
 
@@ -74,12 +73,11 @@
 //         reversed += string[i]; // add each character to the reversed string
 //         i--; // move to the prev character
 //     }
-    
+
 //     return reversed; //return the reversed string
 // }
 
 // console.log(reverseString('Kristy'));
-
 
 //////for loop/////
 
@@ -171,22 +169,19 @@
 // while (num !== null || sum === 0);
 // console.log( 'the total number of your number are ' + sum);
 
-
 //for loop practice/////////////////
 
 // function sumOfSquares(arr) {
 //     let sum = 0;
 //     for (i = 0; i < arr.length; i++) {
-//         sum += arr[i] * arr[i];     
+//         sum += arr[i] * arr[i];
 //     }
 //     return sum;
 // }
 
 // console.log(sumOfSquares([1, 2, 4]));
 
-
 //////////
-
 
 // function countDivisibleByThree(arr) {
 //     sum = 0;
@@ -199,7 +194,6 @@
 // }
 
 // console.log(countDivisibleByThree([1, 2, 3, 4, 5, 6]));
-
 
 ////do...while practice////
 
@@ -220,40 +214,144 @@
 
 ////////
 
-        // function calculateTip(preTip, tipPercent) {
-        //     const tipResult = preTip * tipPercent;
-        //     return tipResult;
-        // }
-        // const preTipTotal = 100.00;
-        // const tipPercentge = 0.15;
-        // const tipCost = calculateTip(preTipTotal, tipPercentge);
-        // const totalBill = preTipTotal + tipCost;
-        // document.write("<br>" + "total bill is: $" + totalBill)
+// function calculateTip(preTip, tipPercent) {
+//     const tipResult = preTip * tipPercent;
+//     return tipResult;
+// }
+// const preTipTotal = 100.00;
+// const tipPercentge = 0.15;
+// const tipCost = calculateTip(preTipTotal, tipPercentge);
+// const totalBill = preTipTotal + tipCost;
+// document.write("<br>" + "total bill is: $" + totalBill)
 //using anonymous function instead
-    //     const preTipTotal = 100.00;
-    //     const tipPercentage = 0.15;
+//     const preTipTotal = 100.00;
+//     const tipPercentage = 0.15;
 
-    //     const tipCost = function(preTip, tipPercent) {
-    //         const tipResult = preTip * tipPercent;
-    //         return (tipResult);
-    //     }
+//     const tipCost = function(preTip, tipPercent) {
+//         const tipResult = preTip * tipPercent;
+//         return (tipResult);
+//     }
 
-    //     const totalBill = preTipTotal + tipCost(preTipTotal, tipPercentage);
-    //     document.write("<br>" + "Your total Bill $" + totalBill);
+//     const totalBill = preTipTotal + tipCost(preTipTotal, tipPercentage);
+//     document.write("<br>" + "Your total Bill $" + totalBill);
 
-    // ////////////////
+// ////////////////
 
+// function makeBodyBlue() {
+//     document.body.style.backgroundColor = 'blue'
+//     console.log("the backgroud is now blue")
+// }
+// function makeBodyPink() {
+//     document.body.style.backgroundColor = 'hotpink'
+// }
 
-         // function makeBodyBlue() {
-      //     document.body.style.backgroundColor = 'blue'
-      //     console.log("the backgroud is now blue")
-      // }
-      // function makeBodyPink() {
-      //     document.body.style.backgroundColor = 'hotpink'
-      // }
+// function changeColors(back, text) {
+//   document.body.style.backgroundColor = back;
+//   document.body.style.color = text;
+// }
+/////////////////////
 
-      // function changeColors(back, text) {
-      //   document.body.style.backgroundColor = back;
-      //   document.body.style.color = text;
-      // }
-      /////////////////////
+///////flatten example///////
+// imagine a big box of toys with lots of toys. some of them are visible as
+// soon as you open the box, the other are nested within other smaller boxes
+// the objective of 'flatten' is to take all the toys in the main box and the
+// subboxes and line them up in one line so everything is visible/in the same array
+// the 'toys' can be numbers, strings, or other simple things that are inside the
+// objects or arrays i.e the main box and the nested boxes.
+
+//the value can be a toy, a box of toys, or a collection of boxes inside more boxes
+//we don't know yet so we use the flatten function.
+
+function flatten(value) {
+  //first check.
+  //is the value given already a 'toy' you are able to see?
+  //ex. a number 5 or a word like 'hello'?
+  if (typeof value !== "object" || value === null) {
+    //if so just return it as is.
+    return value;
+  }
+
+  //second check.
+  //'is this value an array? a box withing the bigger box?
+  if (Array.isArray(value)) {
+    //if so, run the flattenArray() function that will be defined below.
+    return flattenArray(value);
+  }
+
+  //otherwise the value must be an object so return/run the flattenObject() function that will be defined below.
+  return flattenObject(value);
+}
+
+//the flattenArray() function
+// in order to open the 'boxes'/arrays within the main box/array.
+// the reduce method goes through each toy/box one by one putting them all
+// into a single pile by using the concatination.
+// for every item in the array:
+// the function checks if its a simple toy(i.e number or string) or another
+// box using the flatten(curr0 to open the box)
+//finally putting them all into one array/one pile
+
+function flattenArray(array) {
+  return array.reduce((acc, curr) => acc.concat(flatten(curr)), []);
+}
+
+//the flattenObject() function
+// this checks for objects which are another type of 'box'.
+//instead of 'toys' this box has labels for each toy such as "name: Teddy Bear
+// or "color: red".
+// start by init an empty object where the items will be put after flattening.
+// go through each compartment in the box using a for loop.
+//if there is another box in the subbox open it using flatten(value) and look for more things.
+//if its just a simple 'toy' like a number or string we put it in the intit obj.
+//if the item was in another box we put everything from that box into the
+//big box (flattenObj) using Object.assign
+//if it was just simple 'toy' we put it directly into the box and label it
+//(flattnedObj[key] = flattenedValue).
+
+function flattenObject(object) {
+  const flattenedObj = {};
+
+  for (const [key, value] of Object.entries(object)) {
+    const valueIsObject =
+      typeof value === "object" && value !== null && !Array.isArray(value);
+    const flattenedValue = flatten(value);
+
+    if (valueIsObject) {
+      Object.assign(flattenedObj, flattenedValue);
+    } else {
+      flattenedObj[key] = flattenedValue;
+    }
+  }
+  return flattenedObj;
+}
+//test 1
+console.log("test 1: flatten a flat object");
+console.log(flatten({ name: "Alice", age: 25 }));
+
+//test 2
+console.log("test 2: tlatten a nested object");
+console.log(
+  flatten({ name: "alice", details: { age: 25, city: "wonderland" } })
+);
+
+//test 3
+console.log("test 3: flatten a nested array");
+console.log(flatten([1, [2, [3, 4]], 5]));
+
+//test 4
+console.log("test 4: flatten an object with arrays");
+console.log(flatten({ numbers: [1, 2, [3, 4]], name: "alice" }));
+
+//test 5
+console.log("test 5: flatten a primitive value");
+console.log(flatten(42));
+
+//summary
+// the 'flatten()' function decides what to do with each item
+// do i need to open this box, or is this just a 'toy'?
+
+//the 'flattenArray()' function opens boxes (arrays) full of toys and gather
+//everything in one place.
+
+//the 'flattenObject()' function opens boxes (objects) that have labeled compartments
+//making sure to not miss aything hidin in smaller boxes inside
