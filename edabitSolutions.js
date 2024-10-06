ed1a794e36d63f34d74699a19a1c5b347b6d56de
karim <hema_thechamp@yahoo.com>
2024-10-06 19:34:00 -0400

    solved a couple of edabit array looping problems

--- edabitSolutions.js
+++ edabitSolutions.js
@@ -10,48 +10,65 @@
 /////////////////////////
 
 //https://edabit.com/challenge/FipbQSYquQLPZ8QXG
-function isSeven(x) {
-	return x === 7 ? true : false;
-}
-console.log(isSeven(7));
-console.log(isSeven(8)); 
+// function isSeven(x) {
+// 	return x === 7 ? true : false;
+// }
+// console.log(isSeven(7));
+// console.log(isSeven(8)); 
 ////////////////////////////////////////
 
 //https://edabit.com/challenge/KSTkFSnaYBJdo6PHx
-function boolToString(flag) {
-  return flag.toString();
-}
-console.log(boolToString(true));
-console.log(boolToString(false));
+// function boolToString(flag) {
+//   return flag.toString();
+// }
+// console.log(boolToString(true));
+// console.log(boolToString(false));
 
-//https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG
+// //https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG
 
-function sumPolygon(n) {
-  return (
-    (n - 2) * 180
-    )
-}
-console.log(sumPolygon(7));
+// function sumPolygon(n) {
+//   return (
+//     (n - 2) * 180
+//     )
+// }
+// console.log(sumPolygon(7));
 //////////////////////////////
 
 //https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3
-function checkEquality(a, b) {
-  if (a === b) {
-		return true;
-	} else return false;
-}
-console.log(checkEquality(true, 1));
-console.log(checkEquality(1, '1'));
-console.log(checkEquality(1, 1));
+// function checkEquality(a, b) {
+//   if (a === b) {
+// 		return true;
+// 	} else return false;
+// }
+// console.log(checkEquality(true, 1));
+// console.log(checkEquality(1, '1'));
+// console.log(checkEquality(1, 1));
 /////////////////////////////////
 
 //https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3
-function maxNum(n1, n2) {
-	if (n1 > n2) {
-	  return n1
-	} else if (n1 < n2) {
-		return n2
-	}
+// function maxNum(n1, n2) {
+// 	if (n1 > n2) {
+// 	  return n1
+// 	} else if (n1 < n2) {
+// 		return n2
+// 	}
+// }
+// console.log(maxNum(2, 1));
+// console.log(maxNum(1, 2));
+
+///////////////////////////////////
+// https://edabit.com/challenge/RMZiERz2cbjmbXruY
+function triangle(n) {
+	const value = n * (n + 1) / 2;
+	return value;
 }
-console.log(maxNum(2, 1));
-console.log(maxNum(1, 2));
+console.log(triangle(6));
+
+///////////////////////////
+// https://edabit.com/challenge/ZN5cpGPNRS3nrjMo5
+
+function capMe(arr) {
+  return arr.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+}
+
+console.log(capMe(['olivia', 'emily']))
