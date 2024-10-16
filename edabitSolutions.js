670c4df903fa73703dcd8220a6b6e62e30619f2b
karim <hema_thechamp@yahoo.com>
2024-10-15 21:29:35 -0400

    solved an edabit loop challenge

--- edabitSolutions.js
+++ edabitSolutions.js
@@ -327,3 +327,19 @@
 //   return obj;
 // }
 // console.log(addName({}, "Brutus", 300))
+
+/////////////////////
+//https://edabit.com/challenge/b7iHQDw72zzkmgCun
+
+function countBoomerangs(arr) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (arr[i] === arr[i + 2] && arr[i] !== arr[i + 1]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]));
