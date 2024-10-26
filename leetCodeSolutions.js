118de182639ad8d1feddf1a790f2d3dab41b897a
karim <hema_thechamp@yahoo.com>
2024-10-26 13:51:57 -0400

    solved a leetcode challenge roman to integer

--- leetCodeSolutions.js
+++ leetCodeSolutions.js
@@ -39,18 +39,56 @@
 
 //https://leetcode.com/problems/longest-common-prefix/
 
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return ""; //edge case: empty array
-    let prefix = strs[0]; //assume the first string is a prefix
+// var longestCommonPrefix = function(strs) {
+//     if (strs.length === 0) return ""; //edge case: empty array
+//     let prefix = strs[0]; //assume the first string is a prefix
 
-    for (let i = 1; i < strs.length; i++) { //compare starting from second string
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.slice(0, -1); //shorten prefix by removing the last character
-            if (prefix === "") return "";// no common prefix
-        }
+//     for (let i = 1; i < strs.length; i++) { //compare starting from second string
+//         while (strs[i].indexOf(prefix) !== 0) {
+//             prefix = prefix.slice(0, -1); //shorten prefix by removing the last character
+//             if (prefix === "") return "";// no common prefix
+//         }
+//     }
+//     return prefix;
+// };
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+// console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+// console.log(longestCommonPrefix(["test", "test", "test"]));
+
+//////////////////////
+
+//https://leetcode.com/problems/roman-to-integer/
+
+
+var romanToInt = function (s) {
+    //map of roman numeral to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let i = 0;
+
+    while (i < s.length) {
+        const currentVal = romanMap[s[i]];
+        const nextVal = romanMap[s[i + 1]];
+    
+
+    //if current value is less than the next value then subtract it
+    if (nextVal && currentVal < nextVal) {
+        result += nextVal - currentVal;
+        i += 2; //skipthe next character since its already been processed
+    } else {
+        result += currentVal;
+        i += 1;
     }
-    return prefix;
+}
+return result;
+
 };
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
-console.log(longestCommonPrefix(["dog", "racecar", "car"]));
-console.log(longestCommonPrefix(["test", "test", "test"]));
