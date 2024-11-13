a815a9e537fa233a512ca12985b8eaab1545e2a0
karim <hema_thechamp@yahoo.com>
2024-11-12 22:17:50 -0500

    solved 5 CodeWars challenges

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -50,3 +50,77 @@
 //     return n.toString().split('').map(Number).reverse();
 //   }
 //   console.log(digitize(35231));
+
+///////////////////////////////////////////////////
+
+
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
+// function fakeBin(x){
+//     const arr = x.split('').map(Number);
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] < 5) {
+//         arr[i] = 0;
+//       } else if (arr[i] >= 5) {
+//         arr[i] = 1;
+//       }
+//     }
+//     return arr.join('')
+//   }
+
+//   console.log(fakeBin('45385593107843568'))
+
+//////////////////////////////////////////////////////
+
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+// function grow(x){
+//     return x.reduce((acc, curr) => acc * curr, 1)
+//   }
+
+//   console.log(grow([1, 2, 3]))
+
+//////////////////////////////////////////////////////
+
+//https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/javascript
+// const quarterOf = (month) => {
+//     if (month <= 3 && month > 0) {
+//       return 1;
+//     } else if (month <= 6) {
+//       return 2
+//     } else if (month <= 9) {
+//       return 3;
+//     } else if (month <= 12) {
+//       return 4;
+//     } 
+//   }
+
+//   console.log(month(9))
+///////////////////////////////////////////////////////
+
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+// function solution(str){
+//     return str.split('').reverse().join('');
+//   }
+
+// console.log(solution('world'))
+
+/////////////////////////////////////////////////
+
+//https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript
+
+// function firstNonRepeatingLetter(s) {
+//     const lowerCase = s.toLowerCase();
+     
+//      for (let i = 0; i < s.length; i++) {
+//        const char = lowerCase[i];
+       
+//        if (lowerCase.indexOf(char) === lowerCase.lastIndexOf(char)) {
+//          return s[i];
+//        }
+//      }
+//      return "";  
+//    }
+
+//    console.log(firstNonRepeatingLetter('moonmen'))
