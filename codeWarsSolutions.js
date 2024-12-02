4796960cf41832081ef9cb802b115f960705a65d
karim <hema_thechamp@yahoo.com>
2024-12-01 22:28:36 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -774,4 +774,19 @@
 
 ///////////////////////////////////////////////
 
+//https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
+
+// function firstNonConsecutive (arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//       if (arr[i] + 1 !== arr[i + 1]){
+//         return arr[i + 1]
+//     }
+//   }
+//   return null
+//   }
+
+//   console.log(firstNonConsecutive([1, 2, 3, 4, 5]))
+//   console.log(firstNonConsecutive([1, 2, 4, 5, 6, 7]))
+
+/////////////////////////////
 //
