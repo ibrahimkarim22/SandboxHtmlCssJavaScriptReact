50b9c794f5eb4c3559626489cd9f9e678d32fe8d
karim <hema_thechamp@yahoo.com>
2024-12-06 22:07:55 -0500

    solved 1 leetcode challenge

--- leetCodeSolutions.js
+++ leetCodeSolutions.js
@@ -614,5 +614,17 @@
 // console.log(isValid("()[]{}"))
 // console.log(isValid("(]"))
 // console.log(isValid("([])"))
+
 ////////////////////////////
-//
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+// var mergeTwoLists = function(list1, list2) {
+//     const merge = [...list1, ...list2];
+//     return merge.sort((a, b) => a - b)
+// }   
+
+// console.log(mergeTwoLists([1,2,4], [1,3,4]))
+// console.log(mergeTwoLists([], []));
+// console.log(mergeTwoLists([0], [])) 
+
+/////////////////////////////
