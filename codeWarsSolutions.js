4fa3a1f958c4afe34dedd2c189faba64967f3178
karim <hema_thechamp@yahoo.com>
2024-12-25 23:07:23 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1159,4 +1159,20 @@
 
 /////////////////////////////////////////////////////
 
+//https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/javascript
+
+// function powersOfTwo(n) {
+//   let arr = [];
+//   for (let i = 0; i <=n; i++) {
+//     arr.push(2**i);
+//   }
+//   return arr;
+// }
+
+// console.log(powersOfTwo(0));
+// console.log(powersOfTwo(1));
+// console.log(powersOfTwo(4));
+
+
+////////////////////////////
 //
