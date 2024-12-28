e177696fd290b9fb55984c61b0a7b84450229457
karim <hema_thechamp@yahoo.com>
2024-12-27 23:37:40 -0500

    solved 1 CodeWars  Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1195,4 +1195,16 @@
 
 /////////////////////////////
 
+//https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+
+// var number = function(array) {
+//   return array.map((line, index) => `${index + 1}: ${line}`)
+
+// };
+// console.log(number([])); 
+// console.log(number(["a", "b", "c"]));
+
+////////////////////////////
+
 //
+
