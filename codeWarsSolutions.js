10ee05cbf635c2ca2cfa2eb065d60a8901894869
karim <hema_thechamp@yahoo.com>
2024-12-28 23:40:28 -0500

    solved 1 CodeWars challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1206,5 +1206,28 @@
 
 ////////////////////////////
 
+//https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+// function countSheeps(sheep) {
+//   let count = 0;
+
+//   for (let i = 0; i < sheep.length; i++) {
+//     if (sheep[i] === true) {
+//       count++;
+//   } 
+//   return count;
+// }
+// }
+// console.log(countSheeps([]));
+// console.log(countSheeps([undefined]));
+// console.log(countSheeps([null]));
+// console.log(countSheeps([false]));
+// console.log(countSheeps([true]));
+// console.log(countSheeps[undefined, null, false, true]);
+// console.log(countSheeps([undefined, null, false, true, true, false, null, undefined]));
+// console.log(countSheeps[true, true, true, false, true, true, true, true, true, false, true, false])
+
+////////////////////////////////////
+
 //
 
