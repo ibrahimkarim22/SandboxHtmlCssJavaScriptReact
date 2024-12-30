ec67e99ac9f87f0d7b3fdd93f436bf9862b699cf
karim <hema_thechamp@yahoo.com>
2024-12-29 22:17:38 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1229,5 +1229,14 @@
 
 ////////////////////////////////////
 
-//
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
+// function makeNegative(num){
+//   return num > 0 ? -num : num;
+// }
+
+// console.log(makeNegative(7))
+
+////////////////////////////////
+
+//
