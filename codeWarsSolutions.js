3367bfe25fe9183def686c4be2b0fe5bcf691476
karim <hema_thechamp@yahoo.com>
2025-01-01 22:31:00 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1276,4 +1276,18 @@
 
 ////////////////////////////
 
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+// function past(h, m, s) {
+//   let hour = h * 3600000;
+//   let minute = m * 60000;
+//   let second = s * 1000;
+
+//   return hour + minute + second;
+// }
+
+// console.log(past(1, 1, 1));
+
+///////////////////////
+
 //
