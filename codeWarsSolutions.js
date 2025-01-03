83d468266143ddede44025af572db566b885d997
karim <hema_thechamp@yahoo.com>
2025-01-02 22:37:21 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1290,4 +1290,18 @@
 
 ///////////////////////
 
+//https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+// function move(position, roll) {
+//   return position + (roll * 2)
+// }
+
+// console.log(move(0, 4));
+// console.log(move(3, 6));
+// console.log(move(2, 5));
+
+
+///////////////////
 //
+
+
