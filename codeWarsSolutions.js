260b517fff4a8d2cda50ce5d03b8b3f4cc69fb08
karim <hema_thechamp@yahoo.com>
2025-01-03 22:26:35 -0500

    solved 1 CodeWars challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1302,6 +1302,19 @@
 
 
 ///////////////////
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+// var summation = function (num) {
+//   let sum = 0;
+//   for (let i = 1; i <= num; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(summation(1));
+// console.log(summation(2));
+// console.log(summation(8));
+
+//////////////////////////////
 //
-
-
