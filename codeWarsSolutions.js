1426949e53215456c16ed7379aadb150683cc297
karim <hema_thechamp@yahoo.com>
2025-01-13 22:30:51 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1470,7 +1470,21 @@
 
 //////////////////////
 
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+
+// function countBy(x, n) {
+//     let z = [];
+//     for (let i = 1; i <= n; i++) {
+//         z.push(x * i);
+//     }
+//     return z;
+// }
+
+// console.log(countBy(1, 10));
+// console.log(countBy(2, 5));
+
+
+////////////////////////////////////
+
 //
-
-
-
