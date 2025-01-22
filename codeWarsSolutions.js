70b386dd3fb3435008535e7f21948bee6a7ee244
karim <hema_thechamp@yahoo.com>
2025-01-21 21:32:54 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1589,4 +1589,25 @@
 ///////////////////////////////////
 
 
+//https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+// function persistence(num) {
+//   let count = 0;
+ 
+//  while (num >= 10) {
+//    num = num
+//      .toString()
+//      .split('')
+//      .map(Number)
+//      .reduce((a, b) => a * b);
+//    count ++
+//  }
+//  return count
+// }
+
+// console.log(persistence(39))
+// console.log(persistence(4))
+// console.log(persistence(25))
+// console.log(persistence(999))
+
+/////////////////////////////
 //
