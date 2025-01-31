2d9dcd06d0019d5c71a3de40a427a998f0a29381
karim <hema_thechamp@yahoo.com>
2025-01-30 22:26:57 -0500

    solved 1 CodeWars Challenge

--- codeWarsSolutions.js
+++ codeWarsSolutions.js
@@ -1780,4 +1780,17 @@
 
 //////////////////////////////////
 
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+
+// function createPhoneNumber(numbers){
+//   return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6, 10).join('')}`
+// }
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+/////////////////////////////////
+
 //
